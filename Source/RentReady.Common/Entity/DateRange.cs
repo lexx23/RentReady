6f082bf36aa944f93c33df668e239fffb22d5b0f
Alexey Okhotnikov <lexxhunter23@gmail.com>
2022-01-03 15:58:15 +0300

    First code

---
+++ Source/RentReady.Common/Entity/DateRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RentReady.Common.Entity
+{
+    public class DateRange
+    {
+        [JsonProperty(Required = Required.Always)]
+        public DateTime StartOn { get; set; }
+        
+        [JsonProperty(Required = Required.Always)]
+        public DateTime EndOn { get; set; }
+    }
+}
